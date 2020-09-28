9af618a2cbf495650e71142b49f6179cea200758
EC2 Default User <ec2-user@ip-172-31-21-57.ec2.internal>
2020-09-28 13:06:27 +0000

    [Add

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,20 @@
 class BooksController < ApplicationController
+   before_action :authenticate_user!
   def new
-  end
-
-  def create
   end
 
   def index
   end
+  def create
+    @book = book.new(book_params)
+    if @book.save
+    redirect_to book_path(@book.id), notice: 'Your book was successfully created'
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
 
   def show
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/index'
   root to: 'homes#top'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index, :show, :edit]
   resources :books, only: [:new, :create, :index, :show]
 end
