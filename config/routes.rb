980bbfd3186e3411912e2a409774319b91fce7c1
EC2 Default User <ec2-user@ip-172-31-21-57.ec2.internal>
2020-09-29 05:01:28 +0000

    [Add] 4-9 kaminari追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/index'
   root to: 'homes#top'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
